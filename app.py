0cbe6894d58d133eb101f8fea53fc1a7f3e15c20
Nathan <frost.nathan1995@gmail.com>
2022-07-10 22:56:21 -0400

    finished first portion of cupcakes exercise

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.debug = True
+
+app.config['SECRET_KEY'] = '12345'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Cupcake model"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
